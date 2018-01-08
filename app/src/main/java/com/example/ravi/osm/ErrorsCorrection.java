e6993515b81260362115c7f6f3d7de64e8d905ff
Ravi <rvimittal@gmail.com>
2018-01-08 21:35:47 +0530

    changed the references

---
+++ app/src/main/java/com/example/ravi/osm/ErrorsCorrection.java
@@ -0,0 +1,13 @@
+package com.example.ravi.osm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ErrorsCorrection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_errors_correction);
+    }
+}
